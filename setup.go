79a781ea1620d32846316872ee8cada7832f6f0a
Eddie Jibson <jibson@tuta.io>
2019-09-20 17:12:05 +0100

    v4

--- setup.go
+++ setup.go
@@ -29,13 +29,13 @@ var dbConn *mongo.Client
 
 //CertObtained - Called when a certificate for a domain has been obtained su
 func CertObtained(event caddy.EventName, info interface{}) error {
-	fmt.Println(info)
+	fmt.Printf("%v", info)
 	return nil
 }
 
 //CertFailed - guess what this does
 func CertFailed(event caddy.EventName, info interface{}) error {
-	fmt.Println(info)
+	fmt.Printf("%v", info)
 	return nil
 }
 
