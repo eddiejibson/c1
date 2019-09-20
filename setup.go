6865ed3f99dc49399cf75c4bb4c32ff511e24c53
Eddie Jibson <jibson@tuta.io>
2019-09-20 16:33:27 +0100

    tests

--- setup.go
+++ setup.go
@@ -1,16 +1,55 @@
-package certdbb
+package certdb
 
 import (
+	"context"
+	"fmt"
+	"log"
+
 	"github.com/caddyserver/caddy"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+//Cert - Successful cert struct for putting into collection
+type Cert struct {
+	Name string
+	Time string
+}
+
 func init() {
-	caddy.RegisterPlugin("certdbb", caddy.Plugin{
+	caddy.RegisterPlugin("certdb", caddy.Plugin{
 		ServerType: "http",
 		Action:     Setup,
 	})
+	caddy.RegisterEventHook("ondemandcertobtained", CertObtained)
 }
 
+var dbConn *mongo.Client
+
+//CertObtained - Called when a certificate for a domain has been obtained su
+func CertObtained(event caddy.EventName, info interface{}) error {
+	fmt.Println(info)
+	return nil
+}
+
+//Setup function
 func Setup(c *caddy.Controller) error {
+	if !c.NextArg() {
+		return c.ArgErr()
+	}
+	value := c.Val()
+	if len(value) <= 0 {
+		value = "mongodb://mongodb:27017"
+	}
+	clientOptions := options.Client().ApplyURI(value)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to the cert Database")
 	return nil
 }
